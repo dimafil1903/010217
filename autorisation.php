00bdfaec96ea5ab94bf93a119e6ef7ffc0bc672f
levelup <levelup@levelup.com>
2017-02-01 15:29:41 +0200

    dd proverky

--- autorisation.php
+++ autorisation.php
@@ -1,1 +1,8 @@
-
+<?php
+$adminlog='iamadmin';
+$adminpass='iamadmin';
+$login=filter_input(INPUT_POST,'login');
+$pass=filter_input(INPUT_POST,'pass');
+if($adminlog==$login && $adminpass==$pass){
+    echo 'вы авторизированы';
+}
